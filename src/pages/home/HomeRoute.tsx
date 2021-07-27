743d861f24f432bdc7d4cb5276806065a7158472
SenaGust <gustavosena98@live.com>
2021-07-26 23:38:19 -0300

    Fixed eslint errors

--- src/pages/home/HomeRoute.tsx
+++ src/pages/home/HomeRoute.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from '../../assets/logo.svg';
 
-function App() {
+function App(): JSX.Element {
   return (
     <div className="App">
       <header className="App-header">
