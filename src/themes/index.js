a3f9bc5b9973e0f9ef0e974a2b1e9f32c024d238
SenaGust <gustavosena98@live.com>
2021-07-27 23:47:08 -0300

    Fixed some things

--- src/themes/index.js
+++ src/themes/index.js
@@ -10,14 +10,6 @@ const theme = createTheme({
       '@global': {
         body: {
           color: 'rgba(0, 0, 0, 0.87)',
-          lineHeight: '1.43',
-          letterSpacing: '0.01071em',
-          backgroundColor: '#fafafa',
-          fontSize: '0.875rem',
-
-          '&::backdrop': {
-            backgroundColor: '#fafafa',
-          },
         },
       },
     },
