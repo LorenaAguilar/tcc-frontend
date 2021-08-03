b201243fc65c2c5979f4a5fbf105a42af1201a7f
SenaGust <gustavosena98@live.com>
2021-08-02 21:17:48 -0300

    Added jest

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,4 +1,21 @@
 import { configure } from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import React from 'react';
+import { once } from 'lodash';
 
 configure({ adapter: new Adapter() });
+
+jest.spyOn(React, 'useEffect').mockImplementation((f) => once(f));
+jest.spyOn(React, 'useCallback').mockImplementation((f) => once(f));
+jest.spyOn(React, 'useMemo').mockImplementation((f) => once(f));
+
+function mockUseStyles(useStylesFn, classesArray) {
+  useStylesFn.mockReturnValue(
+    classesArray.reduce((classes, className) => {
+      classes[className] = className;
+      return classes;
+    }, {})
+  );
+}
+const _global = (window || global) as any;
+_global.mockUseStyles = mockUseStyles;
