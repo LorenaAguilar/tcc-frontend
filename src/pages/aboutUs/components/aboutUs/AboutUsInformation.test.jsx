dc281104a8fd68c7505c6e36a0a5c1dd5af9668c
SenaGust <gustavosena98@live.com>
2021-08-04 21:40:46 -0300

    Implemented unit test for AboutUsInformation

---
+++ src/pages/aboutUs/components/aboutUs/AboutUsInformation.test.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Box from '@material-ui/core/Box';
+import AboutUsInformation from './AboutUsInformation';
+import Container from '../../../../components/container/Container';
+import AboutUsInformationStyles from './AboutUsInformation.styles';
+
+jest.mock('./AboutUsInformation.styles');
+
+mockUseStyles(AboutUsInformationStyles, ['container']);
+
+describe('AboutUsInformation', () => {
+  it('should render correctly', () => {
+    const wrapper = shallow(<AboutUsInformation />);
+
+    const expectedWrapper = (
+      <Container>
+        <Box className="container" />
+      </Container>
+    );
+    expect(wrapper.matchesElement(expectedWrapper)).toBe(true);
+  });
+});
--- src/pages/aboutUs/components/aboutUs/AboutUsInformation.tsx
+++ src/pages/aboutUs/components/aboutUs/AboutUsInformation.tsx
@@ -1,7 +1,7 @@
-import { Box } from '@material-ui/core';
+import Box from '@material-ui/core/Box';
 import React from 'react';
-import AboutUsInformationStyles from './AboutUsInformation.style';
-import Container from '../../../../components/container/container';
+import AboutUsInformationStyles from './AboutUsInformation.styles';
+import Container from '../../../../components/container/Container';
 
 const AboutUsInformation: React.FunctionComponent = () => {
   const classes = AboutUsInformationStyles();
