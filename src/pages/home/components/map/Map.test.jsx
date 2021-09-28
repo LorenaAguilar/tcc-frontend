2b65ffe9099d100d34d707a4c05e5576f052650c
SenaGust <gustavosena98@live.com>
2021-09-28 20:53:00 -0300

    Trying to solve unit test

--- src/pages/home/components/map/Map.test.jsx
+++ src/pages/home/components/map/Map.test.jsx
@@ -1,31 +1,69 @@
-import React from 'react';
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
 import { shallow } from 'enzyme';
-import MapView from './MapView';
+import React from 'react';
+import Container from '../../../../components/container/container';
+import { DEFAULT_MAP_ZOOM, GOOGLE_API_KEY } from '../../../../constants';
 import Map from './Map';
+import MapViewUseStyles from './MapView.styles';
 
-describe('HomeRoute', () => {
-  it('should render the view correctly with optional props', () => {
-    const mockProps = {
-      location: {
-        lat: 1,
-        lng: 2,
-      },
-      children: <div>test</div>,
+jest.mock('./MapView.styles');
+mockUseStyles(MapViewUseStyles, ['container']);
+
+describe('Map', () => {
+  it('should render the view correctly without getLocation permission', () => {
+    const MockedPosition = {
+      coords: { latitude: 1, longitude: 1 },
+    };
+    const mockGeolocation = {
+      getCurrentPosition: jest.fn().mockImplementation((f) => f(MockedPosition)),
+      watchPosition: jest.fn(),
+    };
+    const mockedChildren = <div>test</div>;
+    const locationBH = {
+      lat: -19.912998,
+      lng: -43.940933,
     };
 
-    const wrapper = shallow(
-      <Map location={mockProps.location}>
-        <div>test</div>
-      </Map>
-    );
+    global.navigator.geolocation = mockGeolocation;
 
-    expect(wrapper.type()).toBe(MapView);
-    expect(wrapper.props()).toEqual({
-      children: <div>test</div>,
-      location: {
-        lat: -3.745,
-        lng: -38.523,
-      },
-    });
+    const wrapper = shallow(<Map>{mockedChildren}</Map>);
+
+    const expectedWrapper = (
+      <Container>
+        <LoadScript googleMapsApiKey={GOOGLE_API_KEY}>
+          <GoogleMap mapContainerClassName="container" center={locationBH} zoom={DEFAULT_MAP_ZOOM}>
+            {mockedChildren}
+          </GoogleMap>
+        </LoadScript>
+      </Container>
+    );
+    console.log(wrapper.debug());
+    expect(wrapper.matchesElement(expectedWrapper)).toBe(true);
+  });
+
+  it('should render the view correctly with getLocation permission', () => {
+    const MockedPosition = {
+      coords: { latitude: 1, longitude: 1 },
+    };
+    const mockGeolocation = {
+      getCurrentPosition: jest.fn(),
+      watchPosition: jest.fn(),
+    };
+
+    global.navigator.geolocation = mockGeolocation;
+    const mockedChildren = <div>test</div>;
+
+    const wrapper = shallow(<Map>{mockedChildren}</Map>);
+    const expectedWrapper = (
+      <Container>
+        <LoadScript googleMapsApiKey={GOOGLE_API_KEY}>
+          <GoogleMap mapContainerClassName="container" center={MockedPosition} zoom={12}>
+            {mockedChildren}
+          </GoogleMap>
+        </LoadScript>
+      </Container>
+    );
+    console.log(wrapper.debug());
+    expect(wrapper.matchesElement(expectedWrapper)).toBe(true);
   });
 });
