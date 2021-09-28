578b87ab648e7c97b44c689751fa84e87c60863d
SenaGust <gustavosena98@live.com>
2021-09-28 20:17:42 -0300

    Fixing component

--- src/pages/home/components/map/Map.tsx
+++ src/pages/home/components/map/Map.tsx
@@ -1,20 +1,26 @@
-import React, { useEffect, useState } from 'react';
-import MapView from './MapView';
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import React, { useEffect, useMemo, useState } from 'react';
+import Container from '../../../../components/container/container';
+import { DEFAULT_MAP_ZOOM, GOOGLE_API_KEY } from '../../../../constants';
+import MapViewUseStyles from './MapView.styles';
 
-interface Props {
-  children?: React.ReactNode;
-}
+const locationBH = {
+  lat: -19.912998,
+  lng: -43.940933,
+};
 
-interface Location {
-  lat: number;
-  lng: number;
-}
+const Map: React.FunctionComponent = ({ children }) => {
+  const { container } = MapViewUseStyles();
 
-function Map({ children }: Props): JSX.Element {
-  const [location, setLocation] = useState<Location>({
-    lat: -3.745,
-    lng: -38.523,
-  });
+  const [location, setLocation] = useState<google.maps.LatLngLiteral>(locationBH);
+
+  const zoom = useMemo(() => {
+    if (location === locationBH) {
+      return 12;
+    }
+
+    return DEFAULT_MAP_ZOOM;
+  }, [location]);
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((position) => {
@@ -25,7 +31,15 @@ function Map({ children }: Props): JSX.Element {
     });
   }, []);
 
-  return <MapView location={location}>{children ?? null}</MapView>;
-}
+  return (
+    <Container>
+      <LoadScript googleMapsApiKey={GOOGLE_API_KEY}>
+        <GoogleMap mapContainerClassName={container} center={location} zoom={zoom}>
+          {children ?? null}
+        </GoogleMap>
+      </LoadScript>
+    </Container>
+  );
+};
 
 export default Map;
