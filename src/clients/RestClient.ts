a30186403c320654b44fbd2eaa7a39e6f290602a
SenaGust <gustavosena98@live.com>
2021-10-10 20:18:31 -0300

    move to prod

--- src/clients/RestClient.ts
+++ src/clients/RestClient.ts
@@ -1,8 +1,14 @@
 import axios, { AxiosInstance } from 'axios';
 
-export default function restClient(
-  baseUrl = 'https://womens-map-service-dev.herokuapp.com/'
-): AxiosInstance {
+const getUrl = () => {
+  if (process.env.NODE_ENV === 'production') {
+    return 'https://womens-map-service.herokuapp.com/';
+  }
+
+  return 'https://womens-map-service-dev.herokuapp.com/';
+};
+
+export default function restClient(baseUrl = getUrl()): AxiosInstance {
   const instance = axios.create({
     baseURL: `${baseUrl}`,
     headers: { 'content-type': 'application/json' },
