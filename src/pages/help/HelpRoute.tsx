c80baeb8f93b7eb9a48bdbc87ce9a31aed7dc9e9
SenaGust <gustavosena98@live.com>
2021-11-16 00:59:21 -0300

    Addde help texts

--- src/pages/help/HelpRoute.tsx
+++ src/pages/help/HelpRoute.tsx
@@ -16,27 +16,27 @@ const HelpRoute: React.FunctionComponent = () => {
       <Box className={classes.information}>
         <TextWithImage
           title="Criação de usuário e Login"
-          text="A aplicação BH mais segura para elas é uma plataforma digital colaborativa que registra locais com baixa iluminação, casos de assédio, assaltos e mortes na cidade de Belo Horizonte."
+          text="Para acessar algumas funcionalidades como criar ocorrências, será necessário possuir uma conta no BH mais segura para elas. Sendo assim, para criar a conta você precisa acessar a opção de número 2 na imagem abaixo. Após isso, você será capaz de logar na aplicação acessando a opção de número 1 na imagem abaixo."
           imagePath={LoginImage}
         />
         <TextWithImage
           title="Mapa"
-          text="A aplicação BH mais segura para elas é uma plataforma digital colaborativa que registra locais com baixa iluminação, casos de assédio, assaltos e mortes na cidade de Belo Horizonte."
+          text="A aplicação BH mais segura para elas possui duas formas de visualização das ocorrências, a primeira é visualizando cada uma das ocorrências. E, a outra é uma visão de ocorrência agrupadas pela localização. A partir desse agrupamento, somos capazes de mostrar quão perigoso é uma área, e assim , quanto mais vermelho é o círculo mais perigosa é a área"
           imagePath={OccurrenceAndRiskZoneImage}
         />
         <TextWithImage
           title="Detalhes de uma ocorrência"
-          text="A aplicação BH mais segura para elas é uma plataforma digital colaborativa que registra locais com baixa iluminação, casos de assédio, assaltos e mortes na cidade de Belo Horizonte."
+          text="Para visualizar as informações de uma ocorrência, você terá que clicar no seu icone dentro do mapa. Após isso, você conseguirá visualizar os detalhes conforme a imagem abaixo."
           imagePath={occurrenceDetailsImage}
         />
         <TextWithImage
           title="Criação de uma ocorrência"
-          text="A aplicação BH mais segura para elas é uma plataforma digital colaborativa que registra locais com baixa iluminação, casos de assédio, assaltos e mortes na cidade de Belo Horizonte."
+          text="Para criar uma ocorrência você precisa acessar a opção do menu ocorrências e clicar no botão indicado pelo número 5"
           imagePath={CreateOccurrenceButtonImage}
         />
         <TextWithImage
           title="Listagem de ocorrências"
-          text="A aplicação BH mais segura para elas é uma plataforma digital colaborativa que registra locais com baixa iluminação, casos de assédio, assaltos e mortes na cidade de Belo Horizonte."
+          text="Por fim, cada uma das ocorrências criadas por você serão exibidas na lista 'Ocorrências cadastradas'. E, a partir dela, você consegue deletar a ocorrência clicando no ícone que é indicado pelo número 6. E, também, caso você queira editar as informação de uma ocorrência, você precisa clicar no ícone indicado pelo número 7."
           imagePath={ListOccurrencesImage}
         />
       </Box>
